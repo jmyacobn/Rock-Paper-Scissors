a7886978a21691b530d7c0afd19efce97df0ea47
Jenn Yacoubian <jyacoubian@gmail.com>
2022-09-22 12:31:12 -0600

    Created game class with select game method

--- src/game.js
+++ src/game.js
@@ -1,1 +1,17 @@
+class Game {
+  constructor(gameSelection) {
+    this.human = new Player("Player", "👩🏽");
+    this.computer = new Player("Computer", "💻");
+    this.gameBoard = [];
+    this.gameSelection = gameSelection;
+    this.fighters = [];
+  }
 
+  selectGame() {
+    if (this.gameSelection === "classic") {
+      this.fighters = ["earth", "water", "fire"]
+    } else {
+      this.fighters = ["earth", "water", "fire", "wind", "space"]
+    };
+  };
+};
