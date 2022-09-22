9f2202216b9d41ecae3d982226da6ddaaefaff50
Jenn Yacoubian <jyacoubian@gmail.com>
2022-09-22 12:30:47 -0600

    Created player class with take turn method

--- src/player.js
+++ src/player.js
@@ -1,1 +1,14 @@
+class Player {
+  constructor(name, token, game) {
+    this.name = name;
+    this.token = token;
+    this.wins = 0;
+    this.fighter = game;
+  }
 
+  takeTurn(game) {
+    var randomIndex = Math.floor(Math.random() * game.fighters.length);
+    this.fighter = game.fighters[randomIndex];
+    return this.fighter;
+  }
+}
