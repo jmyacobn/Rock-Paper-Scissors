54712d2a3c05bfb03a67c5ee539b4eeddb377c1c
Jenn Yacoubian <jyacoubian@gmail.com>
2022-09-23 09:57:28 -0600

    Move track wins to player class and add ideas for check draw, check win, and reset game board to Game class

--- src/game.js
+++ src/game.js
@@ -7,7 +7,8 @@ class Game {
     this.fighters = [];
   }
 
-  selectGame() {
+  selectGame(gameSelection) {
+    this.gameSelection = gameSelection;
     if (this.gameSelection === "classic") {
       this.fighters = ["earth", "water", "fire"]
     } else {
@@ -15,14 +16,40 @@ class Game {
     };
   };
 
-  trackMoves() {
-
-    var humanFighter = this.human.takeTurn("earth")
-    console.log("1st", this.human)
-    // console.log("human", humanFighter)
-    console.log("2nd", this.human.fighter)
-    this.gameBoard.push(this.human.fighter);
-    // this.gameBoard.push(this.computer.takeTurn());
-    return this.gameBoard;
+  checkDraw() {
+    if (this.gameBoard[0] === this.gameBoard[1]) {
+      return true;
+      //dispaly push message in DOM
+      //use timeout
+    } else {
+      return false;
+    };
   };
 };
+
+checkWin() {
+  if () {
+    this.human.wins ++;
+  }
+}
+
+resentGameBoard () {
+  this.gameBoard = [];
+}
+
+// else if (humanFighter === "rock" && computerFighter === "earth" || computerFighter === "wind") || (humanFighter === "water" && computerFighter === "fire" || computerFighter === "space") || (humanFighter === "earth" && computerFighter === "water" || computerFighter === "space") || (humanFighter === "wind" && computerFighter === "water" || computerFighter === "earth") || (humanFighter === "space" && computerFighter === "wind" || computerFighter === "fire") --> human wins
+// else --> computer wins
+
+// human wins//
+//   this.gameBoard[0] === "fire" && this.gameBoard[1] === "earth" || this.gameBoard[1] === "wind"
+// }
+//   this.gameBoard[0] === "water" && this.gameBoard[1] === "fire" || this.gameBoard[1] === "space"
+// }
+//   this.gameBoard[0] === "earth" && this.gameBoard[1] === "water" || this.gameBoard[1] === "space"
+// }
+//   this.gameBoard[0] === "wind" && this.gameBoard[1] === "water" || this.gameBoard[1] === "earth"
+// }
+//   this.gameBoard[0] === "space" && this.gameBoard[1] === "wind" || this.gameBoard[1] === "fire"
+// }
+// this.human.wins ++
+//this.computer.wins ++
